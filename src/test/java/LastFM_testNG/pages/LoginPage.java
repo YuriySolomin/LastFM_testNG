76ff373aa7dbb4026101a31119b2caf256aba390
YuriySolomin <Yuriy.solomin1978@gmail.com>
2015-07-13 14:25:32 +0300

    add LoginPage.java

---
+++ src/test/java/LastFM_testNG/pages/LoginPage.java
@@ -0,0 +1,17 @@
+package LastFM_testNG.pages;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: solomin-y
+ * Date: 7/13/15
+ * Time: 2:24 PM
+
+ */
+public class LoginPage extends Page {
+
+    public LoginPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+}
